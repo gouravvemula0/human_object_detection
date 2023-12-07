2346a1bf2b7706454c6de2961a352de6759f2966
gouravvemula <58952267+gouravvemula0@users.noreply.github.com>
2023-12-07 02:27:08 -0500

    Add files via upload

---
+++ convert_crowdhuman_to_yolov8.py
@@ -0,0 +1,36 @@
+import json
+import cv2
+import os
+import tqdm
+
+def convert_crowdhuman_to_yolov8(annotation_file, output_dir):
+    with open(annotation_file, 'r') as f:
+        annotations = json.load(f)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for annotation in tqdm.tqdm(annotations):
+        image_id = annotation['ID']
+        image_path = os.path.join(os.path.dirname(annotation_file), 'images', image_id)
+        image = cv2.imread(image_path)
+        image_height, image_width = image.shape[:2]
+
+        output_file = os.path.join(output_dir, image_id + '.txt')
+        with open(output_file, 'w') as f:
+            for gtbox in annotation['gtboxes']:
+                if gtbox['tag'] == 'person':
+                    x1, y1, w, h = gtbox['vbox']
+                    x_center = (x1 + w) / 2
+                    y_center = (y1 + h) / 2
+                    normalized_width = w / image_width
+                    normalized_height = h / image_height
+
+                    # Convert to YOLOv8 format
+                    yolo_line = f"{gtbox['tag']} {x_center} {y_center} {normalized_width} {normalized_height}"
+                    f.write(yolo_line + '\n')
+
+# pip install opencv-python tqdm
+
+#python convert_crowdhuman_to_yolov8.py annotation_train.odgt yolov8_train
+#python convert_crowdhuman_to_yolov8.py annotation_val.odgt yolov8_val
